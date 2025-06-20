28bc39f500d04f8967da2c8348de3b19356e0d7a
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-06-20 09:18:39 -0300

    feat: add eletronic class using multiple inheritance

--- aula20/main.py
+++ aula20/main.py
@@ -1,8 +1,8 @@
-from log import LogPrintMixin, LogFileMixin
+from eletronico import Smartphone
 
-lp = LogPrintMixin()
-lp.log_error('qualquer coisa')
-lp.log_success('Que legal')
-lf = LogFileMixin()
-lf.log_error('qualquer coisa')
-lf.log_success('Que legal')
+galaxy_s = Smartphone('Galaxy S')
+iphone = Smartphone('Iphone')
+
+galaxy_s.ligar()
+iphone.ligar()
+
